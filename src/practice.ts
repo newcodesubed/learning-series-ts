7abd9355c6783f92e8c7f64edb2c92e563958f89
Subed <subedshahxi@gmail.com>
2025-07-09 13:36:49 +0545

    feat: function return type

--- src/practice.ts
+++ src/practice.ts
@@ -6,4 +6,18 @@ const myName1 = "Hello";
 //unions
 
 type UserRole = "user" | "admin" | "owner";
-let user: UserRole = "user";
+type User = { username: string; role: UserRole };
+
+const users: User[] = [
+  { username: "john_doe", role: "owner" },
+  { username: "prabin", role: "admin" },
+  { username: "subed", role: "user" },
+];
+
+function fetchUserDetails(username: string): User {
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    throw new Error(`User with username ${username} not Found`);
+  }
+  return user;
+}
