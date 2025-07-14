4ede6c9a98161dfed01555a9850940927ceca18c
Subed <subedshahxi@gmail.com>
2025-07-14 13:11:28 +0545

    used omit type as well

--- src/practice.ts
+++ src/practice.ts
@@ -8,11 +8,14 @@ const myName1 = "Hello";
 type UserRole = "user" | "admin" | "owner";
 type User = { id: number; username: string; role: UserRole };
 
+type UpdatedUser = Partial<User>;
+
+let nextUserId = 1;
 const users: User[] = [
-  { id: 1, username: "john_doe", role: "owner" },
-  { id: 2, username: "prabin", role: "admin" },
-  { id: 3, username: "subed", role: "user" },
-  { id: 4, username: "arbyte", role: "admin" },
+  { id: nextUserId++, username: "john_doe", role: "owner" },
+  { id: nextUserId++, username: "prabin", role: "admin" },
+  // { id: 3, username: "subed", role: "user" },
+  // { id: 4, username: "arbyte", role: "admin" },
 ];
 
 function fetchUserDetails(username: string): User {
@@ -23,7 +26,7 @@ function fetchUserDetails(username: string): User {
   return user;
 }
 
-function updateUser(id: number, updates: any) {
+function updateUser(id: number, updates: UpdatedUser) {
   const foundUser = users.find((user) => user.id === id);
   if (!foundUser) {
     console.error("User not found");
@@ -32,7 +35,20 @@ function updateUser(id: number, updates: any) {
   Object.assign(foundUser, updates);
 }
 
-updateUser(1, { username: "new_prabin" });
-updateUser(4, { role: "owner" });
+// updateUser(1, { username: "new_prabin" });
+// updateUser(4, { role: "owner" });
 
+function addNewUser(newUser: any): User {
+  const user: User = {
+    id: nextUserId++,
+    ...newUser,
+  };
+  users.push(user);
+  return user;
+}
+
+addNewUser({
+  username: "joe_schmoe",
+  role: "user",
+});
 console.log(users);
